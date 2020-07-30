2576935571078dc7cc7624b473bb1c36f77c19c0
Azure Pipelines <azuredevops@microsoft.com>
2020-07-30 22:26:26 +0000

    Deploy to GitHub Pages: 7bcf752ad04793ceb4ff17b08f51c5c3f2a1c745

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.dev3486',
+    VERSION: '3.0.dev3487',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
